eb4056044ae53896121e1e471346392515551ff3
AnjaKoller1996 <anja.koller@uzh.ch>
2021-10-21 12:51:03 +0200

    Prepare comparison plot

--- tezos_analysis.py
+++ tezos_analysis.py
@@ -50,7 +50,7 @@ def calculate_gini_index(wealths):
 
 def compute_gini_all_bakers_per_cycle(start, end):
     gini_indexes_list = []
-    for cycle in range(start, end+1):
+    for cycle in range(start, end + 1):
         rewards = []
         rew = cur.execute('SELECT reward FROM blocks WHERE cycle = %s' % cycle).fetchall()
         for r in rew:
@@ -59,6 +59,13 @@ def compute_gini_all_bakers_per_cycle(start, end):
         gini = calculate_gini_index(rewards)
         gini_indexes_list.append(gini)
     return gini_indexes_list
+
+
+def compute_proportion_snapshot_rolls(start, end):
+    # TODO: corresponds to the fairness plot
+    """ Computed the proportion of rolls in a snapshot compared to all the rolls (for each baker) """
+    proportion_rolls_list = []
+    return proportion_rolls_list
 
 
 def compute_num_working_bakers_per_cycle_list():
@@ -94,6 +101,14 @@ def compute_gini_snapshot_rolls(start, end):
     return gini_indexes_list
 
 
+def compute_proportion_rewards_all_bakers_per_cycle(start, end):
+    """Computes a list for each baker with the proportion of the rolls compared to the total rolls in the respective
+    cycles """
+    # TODO: implement this, corresponds to fairness plot
+    proportion_rolls_list = []
+    return proportion_rolls_list
+
+
 def compute_reward_era_baker_per_cycle(start, end):
     avg_reward_per_cycle = []
     reward_list_tuple = cur.execute('SELECT reward FROM cyclerewards where cycle>=? AND cycle<=?',
@@ -101,6 +116,16 @@ def compute_reward_era_baker_per_cycle(start, end):
     for reward in reward_list_tuple:
         avg_reward_per_cycle.append(reward[0])
     return avg_reward_per_cycle
+
+
+def compute_base_reward_propotion():
+    # TODO: implement the avg/base proportion expected fairness
+    return 1
+
+
+def compute_base_rolls_proportion():
+    # TODO: implement avg/base proportion expected fairness
+    return 1
 
 
 # plot the rewards standard deviations for the bakers for all cycles
@@ -155,7 +180,7 @@ def plot_gini_indexes_rewards_all_bakers_per_cycle(start, end):
     gini_indexes_all_bakers_rewards = compute_gini_all_bakers_per_cycle(start, end)
     y_data_length = len(gini_indexes_all_bakers_rewards)
     # ensure that x_data and y_data have same length (can be different due to extracting it at different times)
-    x_data = list(range(start, end +1))
+    x_data = list(range(start, end + 1))
     y_data = gini_indexes_all_bakers_rewards
     plt.ylim(0.0, 0.1)  # make it the same scale as the plots for the stakes
     plt.plot(x_data, y_data)
@@ -218,6 +243,26 @@ def plot_snapshots_rolls_gini_index(start, end):
     plt.close()
 
 
+def plot_fairness_comparison_rewards_rolls(start, end, alpha):
+    x1_data = list(range(start, end + 1))
+    y1_data = compute_proportion_snapshot_rolls(start, end)
+    # TODO: probably shifted by t-7
+    x2_data = list(range(start, end + 1))
+    y2_data = compute_proportion_rewards_all_bakers_per_cycle(start, end)
+    plt.plot(x1_data, y1_data, label='Rolls')
+    plt.plot(x2_data, y2_data, label='Rewards')
+    base_reward = compute_base_reward_propotion()
+    base_roll = compute_base_rolls_proportion()
+    # TODO: eventually only one line then with two sublines at plus/minus alpha
+    plt.axhline(base_reward, 0, 1, label='Baseline proportion reward', color='red')
+    plt.axhline(base_roll, 0, 1, label='Baseline proportion rolls', color='green')
+    plt.title('Fairness Comparison rewards and rolls proportion from' + str(start) + ' to ' + str(end))
+    plt.xlabel('Cycle')
+    plt.ylabel('Proportion of total rewards/rolls')
+    plt.savefig('images/Fairness_comparison_rewards_rolls_' + str(start) + '_' + str(end) + '.png')
+    plt.close()
+
+
 if __name__ == '__main__':
     # Setup db
     con = sqlite3.connect(DB_FILE)  # attributes: cycle, baker, fee, reward, deposit, blocks (merged db)
@@ -245,9 +290,13 @@ if __name__ == '__main__':
         plot_gini_indexes_rewards_all_bakers_per_cycle(start, end)
 
     # Snapshot rolls gini index plots
-    # TODO: check this does not seem to give a reasonable gini for the rewards -> DEBUG
+    # TODO: check this does not seem to give a reasonable gini for the rewards -> DEBUG -> TOO many snapshots
     for start, end in zip(start_cycles, end_cycles):
         plot_snapshots_rolls_gini_index(start, end)
+
+    # TODO: fairness measure -> in a cycle compare proportion of rolls and proportion of rewards -> define an "ok
+    #  band" and a stochastic variation which is ok
+    plot_fairness_comparison_rewards_rolls(250, 251, 0.03)  # look at cycle 250 and have a variation of 3% tolerance
 
     # Baker rewards per era
     # cycle_names = ['Athens', 'Babylon', 'Carthage', 'Delphi', 'Edo']
@@ -260,3 +309,7 @@ if __name__ == '__main__':
 
     # Close connection
     con.close()
+
+# Note: The right to publish a block in cycle n is assigned to a randomly selected roll in a randomly selected
+# roll snapshot from cycle n - PRESERVED_CYCLES — 2 (which is n-7)
+# roll n-7 must be somehow coherent with rewards n (rewards are payed out 7 cycles after snapshot is taken)
